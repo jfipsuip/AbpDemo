66205c6a282e20dba8faf68b4d19f8c4590f7735
linyitang <linyitang@smartcloudgis.com>
2019-04-23 14:07:19 +0800

    AbpDemo.Migrator.csproj 由类库修改为控制台应该程序

---
+++ aspnet-core/src/AbpDemo.Migrator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbpDemo.Migrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
