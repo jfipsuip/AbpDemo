171e5de41c22c479596d79515c7eb99e8fd0b55d
linyitang <linyitang@smartcloudgis.com>
2019-04-29 08:53:16 +0800

    添加 Migration 命令需要的文件

---
+++ aspnet-core/src/AbpDemo.EntityFrameworkCore/EntityFrameworkCore/AbpDemoDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AbpDemo.EntityFrameworkCore
+{
+    /* This class is needed to run "dotnet ef ..." commands from command line on development. Not used anywhere else */
+    public class AbpDemoDbContextFactory : IDesignTimeDbContextFactory<AbpDemoDbContext>
+    {
+        public AbpDemoDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<AbpDemoDbContext>();
+
+            AbpDemoDbContextConfigurer.Configure(builder, "Server=localhost; Database=AbpDemoDb; Trusted_Connection=True;");
+
+            return new AbpDemoDbContext(builder.Options);
+        }
+    }
+}
