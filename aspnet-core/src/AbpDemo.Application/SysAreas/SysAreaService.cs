18ac72b75f573467b555698f19207c723f091d3b
linyitang <linyitang@smartcloudgis.com>
2019-04-29 09:02:29 +0800

    添加 测试服务

---
+++ aspnet-core/src/AbpDemo.Application/SysAreas/SysAreaService.cs
@@ -0,0 +1,23 @@
+﻿
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using AbpDemo.SysAreas.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbpDemo.SysAreas
+{
+    /// <summary>
+    /// 区划管理
+    /// </summary>
+    public class SysAreaService : AsyncCrudAppService<SysArea, SysAreaDto, Guid, PagedAndSortedResultRequestDto, SysAreaCDto, SysAreaUDto, EntityDto<Guid>, EntityDto<Guid>>, ISysAreaService
+    {
+
+        public SysAreaService(IRepository<SysArea, Guid> repository) : base(repository)
+        {
+
+        }
+    }
+}
