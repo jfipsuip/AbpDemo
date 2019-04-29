ac3b9d4b6ad6796df0d301fabbb7968dec59a7de
linyitang <linyitang@smartcloudgis.com>
2019-04-29 09:09:51 +0800

    修改 Program.cs 和 Startup.cs 的位置

--- aspnet-core/src/AbpDemo.Web.Host/Startup/Program.cs
+++ aspnet-core/src/AbpDemo.Web.Host/Startup/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace AbpDemo.Web.Host
+namespace AbpDemo.Web.Host.Startup
 {
     public class Program
     {
