d5e1a51795f2b226f758c9ec18c78ec831d44ddd
linyitang <linyitang@smartcloudgis.com>
2019-04-29 09:10:24 +0800

    添加 Moudle 和 Consts 文件

---
+++ aspnet-core/src/AbpDemo.Application/AbpDemoApplicationModule.cs
@@ -0,0 +1,12 @@
+﻿using Abp.AutoMapper;
+using Abp.Modules;
+
+namespace AbpDemo
+{
+    [DependsOn(
+        typeof(AbpDemoCoreModule),
+        typeof(AbpAutoMapperModule))]
+    public class AbpDemoApplicationModule : AbpModule
+    {
+    }
+}
