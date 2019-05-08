361f1ee4335c6160cd1bc630a688ef02959f3ff8
linyitang <linyitang@smartcloudgis.com>
2019-05-08 15:47:39 +0800

    删除可删除的代码

--- aspnet-core/src/AbpDemo.Web.Host/Startup/Startup.cs
+++ aspnet-core/src/AbpDemo.Web.Host/Startup/Startup.cs
@@ -1,66 +1,44 @@
-﻿using Abp.AspNetCore;
-using Abp.Castle.Logging.Log4Net;
-using AbpDemo.Configuration;
-using AbpDemo.EntityFrameworkCore;
-using Castle.Facilities.Logging;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Cors.Internal;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Castle.Facilities.Logging;
 using Swashbuckle.AspNetCore.Swagger;
-using System;
+using Abp.AspNetCore;
+using Abp.Castle.Logging.Log4Net;
+using Abp.Extensions;
+using AbpDemo.Configuration;
+//using AbpDemo.Identity;
 
+using Abp.AspNetCore.SignalR.Hubs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AbpDemo.Web.Host.Startup
 {
     public class Startup
     {
-        private const string _defaultCorsPolicyName = "localhost";
-
-        private readonly IConfigurationRoot _appConfiguration;
-
-        private readonly string _webRootPath;
-
         public Startup(IHostingEnvironment env)
         {
-            _appConfiguration = env.GetAppConfiguration();
-            _webRootPath = env.WebRootPath;
         }
 
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddMvc(
-                options => options.Filters.Add(new CorsAuthorizationFilterFactory(_defaultCorsPolicyName))
-            );
-
-
-            services.AddCors(
-                options => options.AddPolicy(
-                    _defaultCorsPolicyName,
-                    builder => builder
-                        .AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader()
-                        .AllowCredentials()
-                )
-            );
+            // Swagger - Enable this line and the related lines in Configure method to enable swagger UI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
-                c.DocInclusionPredicate((docName, description) =>true);
+                c.DocInclusionPredicate((docName, description) => true);
             });
 
-            //注入AbpDemoDbContext
-            //var configuration = AppConfigurations.Get(WebContentDirectoryFinder.CalculateContentRootFolder());
-            services.AddDbContext<AbpDemoDbContext>(options => options.UseSqlServer(_appConfiguration["ConnectionStrings:Default"]));
-
             // Configure Abp and Dependency Injection
-            return services.AddAbp<AbpDemoWebModule>(
+            return services.AddAbp<AbpDemoWebHostModule>(
                 // Configure Log4Net logging
                 options => options.IocManager.IocContainer.AddFacility<LoggingFacility>(
                     f => f.UseAbpLog4Net().WithConfig("log4net.config")
@@ -70,36 +48,17 @@ namespace AbpDemo.Web.Host.Startup
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseAbp(options => { options.UseAbpRequestLocalization = false; }); // Initializes ABP framework.
-
-            //app.UseCors(); // Enable CORS!
-            app.UseCors(_defaultCorsPolicyName); // Enable CORS!
-
-            app.UseStaticFiles();
-
-            app.UseAuthentication();//声明支持
-
-            //app.UseJwtTokenMiddleware(); //中间件
-
-            app.UseAbpRequestLocalization();
-
-            //app.UseContentRoot(Directory.GetCurrentDirectory())
-
-            //app.UseDiscoveryClient();
-
-
+            // Initializes ABP framework.
+            app.UseAbp(options => { options.UseAbpRequestLocalization = false; }); 
 
             app.UseMvc(routes =>
             {
-                routes.MapRoute(
-                    name: "defaultWithArea",
-                    template: "{area}/{controller=Home}/{action=Index}/{id?}");
-
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
