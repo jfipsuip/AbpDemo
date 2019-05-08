2e77e0a89904dbfd0515010c12452991182b856e
linyitang <linyitang@smartcloudgis.com>
2019-05-08 11:41:21 +0800

    修改自动跳转到 swagger 页面

--- aspnet-core/src/AbpDemo.Web.Host/Startup.cs
+++ aspnet-core/src/AbpDemo.Web.Host/Startup.cs
@@ -42,7 +42,12 @@ namespace AbpDemo.Web.Host
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
