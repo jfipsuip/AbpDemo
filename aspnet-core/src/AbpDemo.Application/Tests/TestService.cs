0e1bebce2f8f0fe7a7928575f2c65e7a9ecc9910
linyitang <linyitang@smartcloudgis.com>
2019-05-10 16:28:01 +0800

    添加测试服务

---
+++ aspnet-core/src/AbpDemo.Application/Tests/TestService.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbpDemo.Tests
+{
+    public class TestService : ApplicationService
+    {
+        public int TestAAA(int n)
+        {
+            return n * n;
+        }
+    }
+}
