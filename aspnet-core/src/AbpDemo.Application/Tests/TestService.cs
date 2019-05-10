32fdcd4b39d27d90413935c6ead9561d972c67e4
linyitang <linyitang@smartcloudgis.com>
2019-05-10 17:20:38 +0800

    添加测试服务

--- aspnet-core/src/AbpDemo.Application/Tests/TestService.cs
+++ aspnet-core/src/AbpDemo.Application/Tests/TestService.cs
@@ -1,4 +1,5 @@
 ﻿using Abp.Application.Services;
+using Abp.Auditing;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,12 @@ namespace AbpDemo.Tests
     public class TestService : ApplicationService
     {
         public int TestAAA(int n)
+        {
+            //throw new Exception("");
+            return n * n;
+        }
+        [DisableAuditing]
+        public int TestBBB(int n)
         {
             return n * n;
         }
