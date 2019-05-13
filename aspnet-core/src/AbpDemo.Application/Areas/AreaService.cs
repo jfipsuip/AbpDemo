628cbc9dc821fbf205c63727479e2bff0602e2bc
linyitang <linyitang@smartcloudgis.com>
2019-05-13 15:05:10 +0800

    添加 服务

---
+++ aspnet-core/src/AbpDemo.Application/Areas/AreaService.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using AbpDemo.Areas.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbpDemo.Areas
+{
+    public class AreaService : AsyncCrudAppService<Area, AreaDto, Guid, PagedAndSortedResultRequestDto, CreateAreaDto, UpdateAreaDto, EntityDto<Guid>, EntityDto<Guid>>
+    {
+        public AreaService(IRepository<Area, Guid> repository) : base(repository)
+        {
+        }
+    }
+}
