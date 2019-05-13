a826c95729178941cd66f7260ad1d9a9da3aad5c
linyitang <linyitang@smartcloudgis.com>
2019-05-13 15:04:05 +0800

    添加测试实体

---
+++ aspnet-core/src/AbpDemo.Core/Areas/Area.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbpDemo.Areas
+{
+    /// <summary>
+    /// 行政区划
+    /// </summary>
+    public class Area : Entity<Guid>
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 行政区划代码
+        /// </summary>
+        public string AreaCode { get; set; }
+    }
+}
