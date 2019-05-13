433419dda55b84deed290f3f2911c996d07270ec
linyitang <linyitang@smartcloudgis.com>
2019-05-13 14:32:38 +0800

    添加 AbpDemoDbContext.cs 使 add-migration 命令能够使用

---
+++ aspnet-core/src/AbpDemo.EntityFrameworkCore/EntityFrameworkCore/AbpDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Abp.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbpDemo.EntityFrameworkCore
+{
+    public class AbpDemoDbContext : AbpDbContext
+    {
+        /* Define a DbSet for each entity of the application */
+        public AbpDemoDbContext(DbContextOptions<AbpDemoDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ aspnet-core/src/AbpDemo.EntityFrameworkCore/EntityFrameworkCore/AbpDemoDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace AbpDemo.EntityFrameworkCore
+{
+    /* This class is needed to run "dotnet ef ..." commands from command line on development. Not used anywhere else */
+    public class AbpDemoDbContextFactory : IDesignTimeDbContextFactory<AbpDemoDbContext>
+    {
+        public AbpDemoDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<AbpDemoDbContext>();
+
+            AbpDemoDbContextConfigurer.Configure(builder, "Server=localhost; Database=AbpDemoDb; Trusted_Connection=True;");
+
+            return new AbpDemoDbContext(builder.Options);
+        }
+    }
+}
