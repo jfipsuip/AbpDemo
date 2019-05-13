c221c302acda75809e324ba479f3463eae522534
linyitang <linyitang@smartcloudgis.com>
2019-05-13 15:05:28 +0800

    添加 MapProfile

---
+++ aspnet-core/src/AbpDemo.Application/Areas/Dto/AreaMapProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbpDemo.Areas.Dto
+{
+    public class AreaMapProfile : Profile
+    {
+        public AreaMapProfile()
+        {
+
+            CreateMap<CreateAreaDto, Area>();
+            CreateMap<UpdateAreaDto, Area>();
+
+            CreateMap<Area, AreaDto>();
+        }
+    }
+}
