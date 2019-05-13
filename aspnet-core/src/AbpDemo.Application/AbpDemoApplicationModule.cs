ef75605911a0bb558b6a7e249a68f4961e86983b
linyitang <linyitang@smartcloudgis.com>
2019-05-13 14:52:02 +0800

    设置  AbpAutoMapperModule 使 继承 Profile 能够按配置映射

--- aspnet-core/src/AbpDemo.Application/AbpDemoApplicationModule.cs
+++ aspnet-core/src/AbpDemo.Application/AbpDemoApplicationModule.cs
@@ -1,15 +1,24 @@
-﻿using Abp.Modules;
+﻿using Abp.AutoMapper;
+using Abp.Modules;
 using Abp.Reflection.Extensions;
 
 namespace AbpDemo
 {
     [DependsOn(
-        typeof(AbpDemoCoreModule))]
+        typeof(AbpDemoCoreModule),
+        typeof(AbpAutoMapperModule))]
     public class AbpDemoApplicationModule : AbpModule
     {
         public override void Initialize()
         {
-            IocManager.RegisterAssemblyByConvention(typeof(AbpDemoApplicationModule).GetAssembly());
+            var thisAssembly = typeof(AbpDemoApplicationModule).GetAssembly();
+
+            IocManager.RegisterAssemblyByConvention(thisAssembly);
+
+            Configuration.Modules.AbpAutoMapper().Configurators.Add(
+                // Scan the assembly for classes which inherit from AutoMapper.Profile
+                cfg => cfg.AddProfiles(thisAssembly)
+            );
         }
     }
 }
