b3e9c4ada57e9b39f19542cf0bc84a2d5be95173
linyitang <linyitang@smartcloudgis.com>
2019-05-14 17:20:58 +0800

    审计日志

---
+++ aspnet-core/src/AbpDemo.EntityFrameworkCore/EntityFrameworkCore/Mapper/AuditLogMapper.cs
@@ -0,0 +1,22 @@
+﻿using AbpDemo.Areas;
+using AbpDemo.AuditLogs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbpDemo.EntityFrameworkCore.Mapper
+{
+    public partial class AuditLogMapper : IEntityTypeConfiguration<AuditLog>
+    {
+        public void Configure(EntityTypeBuilder<AuditLog> builder)
+        {
+            builder.ToTable("Sys_AuditLog");
+
+            // Primary Key
+            builder
+                .HasKey(t => t.Id);
+        }
+    }
+}
