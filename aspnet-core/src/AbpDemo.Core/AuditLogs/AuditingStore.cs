6a1440f8b088475b0e2549dcb4d27364ba92cbad
linyitang <linyitang@smartcloudgis.com>
2019-05-21 10:29:07 +0800

    添加 IAuditingStore 添加审计日志

---
+++ aspnet-core/src/AbpDemo.Core/AuditLogs/AuditingStore.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Auditing;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbpDemo.AuditLogs
+{
+    /// Implements <see cref="IAuditingStore"/> to save auditing informations to database.
+    /// </summary>
+    public class AuditingStore : IAuditingStore, ITransientDependency
+    {
+        public IRepository<AuditLog, long> AuditLogR { get; set; }
+
+        public virtual async Task SaveAsync(AuditInfo auditInfo)
+        {
+            await AuditLogR.InsertAsync(AuditLog.CreateFromAuditInfo(auditInfo));
+        }
+    }
+}
