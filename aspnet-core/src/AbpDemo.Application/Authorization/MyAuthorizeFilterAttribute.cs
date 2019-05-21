6c73eb01107cbe03d93a848cb32ebdf15383c854
linyitang <linyitang@smartcloudgis.com>
2019-05-21 14:09:36 +0800

    添加其它可用的服务

--- aspnet-core/src/AbpDemo.Application/Authorization/MyAuthorizeFilterAttribute.cs
+++ aspnet-core/src/AbpDemo.Application/Authorization/MyAuthorizeFilterAttribute.cs
@@ -2,14 +2,36 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AbpDemo.Authorization
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
     public class MyAuthorizeFilterAttribute : ActionFilterAttribute
     {
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        public override void OnActionExecuting(ActionExecutingContext context)
         {
+            base.OnActionExecuting(context);
+        }
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            return base.OnActionExecutionAsync(context, next);
+        }
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            base.OnResultExecuting(context);
+        }
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            base.OnResultExecuted(context);
+        }
+        public override Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            return base.OnResultExecutionAsync(context, next);
         }
     }
 }
