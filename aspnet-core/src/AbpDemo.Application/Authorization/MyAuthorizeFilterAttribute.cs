220f856afce84ed78b3a3882a1ef3224f6622275
linyitang <linyitang@smartcloudgis.com>
2019-05-21 13:22:21 +0800

    添加  对 ActionFilterAttribute 的重写，在执行 服务前检查

---
+++ aspnet-core/src/AbpDemo.Application/Authorization/MyAuthorizeFilterAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbpDemo.Authorization
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class MyAuthorizeFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+        }
+    }
+}
