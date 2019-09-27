f5caeccf519c328c84191f6810bdf18211c79231
jfipsuip <jfipsuip@126.com>
2019-09-27 16:01:46 +0800

    Migrations/20190927073321_initial.cs

---
+++ aspnet-core/src/AbpDemo.EntityFrameworkCore/Migrations/20190927073321_initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbpDemo.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileInfo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FullName = table.Column<string>(maxLength: 300, nullable: true),
+                    Name = table.Column<string>(maxLength: 100, nullable: true),
+                    Extension = table.Column<string>(maxLength: 100, nullable: true),
+                    Length = table.Column<long>(nullable: false),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    LastWriteTime = table.Column<DateTime>(nullable: false),
+                    LastAccessTime = table.Column<DateTime>(nullable: false),
+                    SHA1 = table.Column<string>(maxLength: 36, nullable: true),
+                    TimeSpan = table.Column<TimeSpan>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileInfo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileInfo");
+        }
+    }
+}
