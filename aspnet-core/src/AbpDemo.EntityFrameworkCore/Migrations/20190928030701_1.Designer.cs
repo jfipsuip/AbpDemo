7e234386c1857268514ea5f0d7a4a66494e79191
jfipsuip <jfipsuip@126.com>
2019-09-28 11:08:14 +0800

    Migrations/20190928030701_1.cs

--- aspnet-core/src/AbpDemo.EntityFrameworkCore/Migrations/20190928030701_1.Designer.cs
+++ aspnet-core/src/AbpDemo.EntityFrameworkCore/Migrations/20190928030701_1.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AbpDemo.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AbpDemo.Migrations
 {
     [DbContext(typeof(AbpDemoDbContext))]
-    partial class AbpDemoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190928030701_1")]
+    partial class _1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/AbpDemo.EntityFrameworkCore/Migrations/20190928030701_1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbpDemo.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SHA1",
+                table: "FileInfo",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 36,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SHA1",
+                table: "FileInfo",
+                maxLength: 36,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
