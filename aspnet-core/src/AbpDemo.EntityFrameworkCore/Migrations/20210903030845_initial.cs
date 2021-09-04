401ba2cfe6dbefe6416a1d4f3535e84073eeb3ad
jfipsuip <jfipsuip@126.com>
2021-09-04 15:44:34 +0800

    1

---
+++ aspnet-core/src/AbpDemo.EntityFrameworkCore/Migrations/20210903030845_initial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbpDemo.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sys_Area",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: true),
+                    AreaCode = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sys_Area", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sys_Dept",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: true),
+                    Code = table.Column<string>(maxLength: 100, nullable: true),
+                    AreaId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sys_Dept", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sys_Dept_Sys_Area_AreaId",
+                        column: x => x.AreaId,
+                        principalTable: "Sys_Area",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sys_Dept_AreaId",
+                table: "Sys_Dept",
+                column: "AreaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sys_Dept");
+
+            migrationBuilder.DropTable(
+                name: "Sys_Area");
+        }
+    }
+}
